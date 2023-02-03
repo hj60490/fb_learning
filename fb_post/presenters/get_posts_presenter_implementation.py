bf542027b8f220fa369bad3eeb25e3699b643218
hj60490 <harshj60490@gmail.com>
2023-02-03 16:46:09 +0530

    presenter implementation exception

--- fb_post/presenters/get_posts_presenter_implementation.py
+++ fb_post/presenters/get_posts_presenter_implementation.py
@@ -1,6 +1,15 @@
 from fb_post.interactors.presenter_interfaces.get_posts_presenter_interface \
     import GetPostsPresenterInterface
+from django_swagger_utils.drf_server.exceptions import (
+    BadRequest
+)
+from fb_post.constants.exception_messages import INVALID_USER_ID
 
 
 class GetPostsPresenterImplementation(GetPostsPresenterInterface):
-    pass
+
+    def raise_exception_for_user_not_exist(self):
+        return BadRequest(*INVALID_USER_ID)
+
+
+
