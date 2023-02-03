30afa0ab48c951b25eaf2003f908450283db58ce
hj60490 <harshj60490@gmail.com>
2023-02-03 09:02:29 +0530

    invalid user exception

--- fb_post/presenters/presenter_implementation.py
+++ fb_post/presenters/presenter_implementation.py
@@ -1,11 +1,12 @@
 from fb_post.interactors.presenter_interfaces.presenter_interface import \
     PresenterInterface
+from fb_post.exceptions.custom_exceptions import InvalidUserException
 
 
 class PresenterImplementation(PresenterInterface):
 
     def raise_exception_for_user_not_exist(self):
-        raise ValueError("User id not found")
+        raise InvalidUserException
 
 
 
