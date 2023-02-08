ba6ecf625213d17ba52b629be352b6b65c978d59
hj60490 <harshj60490@gmail.com>
2023-02-08 16:06:31 +0530

    test for create post wrapper

--- fb_post/tests/interactors/test_create_post_interactor.py
+++ fb_post/tests/interactors/test_create_post_interactor.py
@@ -2,7 +2,8 @@ import pytest
 from unittest.mock import create_autospec
 from fb_post.interactors.storage_interfaces.user_interface import UserInterface
 from fb_post.interactors.storage_interfaces.post_interface import PostInterface
-from fb_post.interactors.presenter_interfaces.presenter_interface import CreatePostPresenterInterface
+from fb_post.interactors.presenter_interfaces.presenter_interface import \
+    CreatePostPresenterInterface
 from fb_post.interactors.create_post_interactor import CreatePostInteractor
 from django_swagger_utils.drf_server.exceptions import BadRequest
 
@@ -29,3 +30,21 @@ def test_create_post_interactor_when_user_not_found_raise_exception():
 
     user_storage.check_is_user_exists.assert_called_once_with(user_id=user_id)
     presenter.raise_exception_for_user_not_exist.assert_called_once()
+
+
+def test_create_post_interactor_with_valid_details_creates_post():
+    user_id = 1
+    content = "hello"
+    user_storage = create_autospec(UserInterface)
+    post_storage = create_autospec(PostInterface)
+    presenter = create_autospec(CreatePostPresenterInterface)
+    interactor = CreatePostInteractor(
+        user_storage=user_storage,
+        post_storage=post_storage,
+        presenter=presenter
+    )
+    user_storage.check_is_user_exists.return_value = True
+
+    assert interactor.create_post_wrapper(user_id=user_id, content=content) is None
+
+
