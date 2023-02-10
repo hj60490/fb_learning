582d3771252aca75897350e02045609ea7580d9b
hj60490 <harshj60490@gmail.com>
2023-02-10 15:55:59 +0530

    some

--- fb_post/tests/storages/test_post_storage_implementation.py
+++ fb_post/tests/storages/test_post_storage_implementation.py
@@ -20,8 +20,9 @@ def test_get_all_posts_with_limit_offset_posts_return_posts_dto(users, posts,
 
 @freeze_time("2023-02-08 11:57:29")
 @pytest.mark.django_db
-def test_get_all_posts_posts_with_post_content_return_posts_dto(users, posts, posts_details_dto_for_content,
-                                              request_parameters_dto_for_content):
+def test_get_all_posts_posts_with_post_content_return_posts_dto(users, posts,
+                                                                posts_details_dto_for_content,
+                                                                request_parameters_dto_for_content):
     user_id = 1
     posts_details_dto_for_content[0].posted_at = "2023-02-08 11:57:29"
     expected_output = posts_details_dto_for_content
@@ -50,20 +51,6 @@ def test_create_post_weather_post_created(users):
     post_storage = PostStorageImplementation()
     post_storage.create_post(content=content, user_id=user_id)
     assert Post.objects.filter(content=content, posted_by_id=1).exists()
-
-#
-# @pytest.mark.django_db
-# def test_get_all_posts_with_post_content_posts_return_posts_dto(
-#         users, posts, request_parameters_dto1, posts_details_with_all_filter
-#
-# ):
-#     user_id = 1
-#     expected_output = posts_details_with_all_filter
-#     post_storage = PostStorageImplementation()
-#     actual_output = post_storage.get_posts(user_id=user_id,
-#                                            requests_parameters_dto=request_parameters_dto1)
-#
-#     assert actual_output == expected_output
 
 
 @pytest.mark.django_db
@@ -126,5 +113,3 @@ def test_get_reactions_on_comments_return_empty(users, posts, comments):
     actual_output = post_storage.get_reactions_on_comments(
         list_of_comment_id=comments_id)
     assert actual_output == expected_output
-
-
