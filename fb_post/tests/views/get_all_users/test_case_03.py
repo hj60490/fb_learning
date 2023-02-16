477141c2f02a32b7cade4b915c73168c0d293975
hj60490 <harshj60490@gmail.com>
2023-02-16 16:18:21 +0530

    views tests done

--- fb_post/tests/views/get_all_users/test_case_03.py
+++ fb_post/tests/views/get_all_users/test_case_03.py
@@ -1,12 +1,12 @@
 """
-# offset is not given
+# limit is not given
 """
 import pytest
 from django_swagger_utils.utils.test_utils import TestUtils
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
 
 
-class TestCase01GetAllUsersAPITestCase(TestUtils):
+class TestCase03GetAllUsersAPITestCase(TestUtils):
     APP_NAME = APP_NAME
     OPERATION_NAME = OPERATION_NAME
     REQUEST_METHOD = REQUEST_METHOD
@@ -17,7 +17,7 @@ class TestCase01GetAllUsersAPITestCase(TestUtils):
     def test_case(self, snapshot):
         body = {}
         path_params = {}
-        query_params = {'limit': 772}
+        query_params = {'limit': -1, 'offset': 0}
         headers = {}
         response = self.make_api_call(body=body,
                                       path_params=path_params,
