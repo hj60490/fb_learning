25d85ba24a5cbc8a938ceab15ac2cc76a2d69ff4
hj60490 <harshj60490@gmail.com>
2023-02-20 08:53:29 +0530

    corrections in models

--- fb_post/models/reaction_storage.py
+++ fb_post/models/reaction_storage.py
@@ -1,10 +1,14 @@
 from django.db import models
 from datetime import datetime
 
+from fb_post.models import Post, Comment
+
 
 class React(models.Model):
-    post_id = models.IntegerField(null=True, blank=True)
-    comment = models.IntegerField(null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             null=True, blank=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE,
+                                null=True, blank=True)
     reaction = models.TextField(max_length=100)
     reacted_at = models.DateTimeField(default=datetime.now)
     reacted_by_id = models.IntegerField(null=True, blank=True)
