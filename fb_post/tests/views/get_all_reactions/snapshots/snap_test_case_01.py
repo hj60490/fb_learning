760b0caf8873e3c4e5b9fa2d203b06ff3ed389c8
hj60490 <harshj60490@gmail.com>
2023-02-21 10:09:10 +0530

    improve test valid test case for tests

--- fb_post/tests/views/get_all_reactions/snapshots/snap_test_case_01.py
+++ fb_post/tests/views/get_all_reactions/snapshots/snap_test_case_01.py
@@ -11,5 +11,68 @@ snapshots['TestCase01GetAllReactionsAPITestCase.test_case status_code'] = '200'
 
 snapshots['TestCase01GetAllReactionsAPITestCase.test_case body'] = {
     'reactions_details': [
+        {
+            'comment': None,
+            'post': {
+                'content': 'post_1',
+                'post_id': 1,
+                'posted_by': {
+                    'name': 'User_1',
+                    'profile_pic': 'https://profile_pic_url1.com',
+                    'user_id': 1
+                }
+            },
+            'reaction': 'SAD',
+            'reaction_id': 1,
+            'reply': None
+        },
+        {
+            'comment': {
+                'comment_id': 1,
+                'commented_by': {
+                    'name': 'User_1',
+                    'profile_pic': 'https://profile_pic_url1.com',
+                    'user_id': 1
+                },
+                'content': 'comment_1',
+                'post': {
+                    'content': 'post_1',
+                    'post_id': 1,
+                    'posted_by': {
+                        'name': 'User_1',
+                        'profile_pic': 'https://profile_pic_url1.com',
+                        'user_id': 1
+                    }
+                }
+            },
+            'post': None,
+            'reaction': 'SAD',
+            'reaction_id': 2,
+            'reply': None
+        },
+        {
+            'comment': {
+                'comment_id': 2,
+                'commented_by': {
+                    'name': 'User_2',
+                    'profile_pic': 'https://profile_pic_url2.com',
+                    'user_id': 2
+                },
+                'content': 'comment_2',
+                'post': {
+                    'content': 'post_2',
+                    'post_id': 2,
+                    'posted_by': {
+                        'name': 'User_2',
+                        'profile_pic': 'https://profile_pic_url2.com',
+                        'user_id': 2
+                    }
+                }
+            },
+            'post': None,
+            'reaction': 'SAD',
+            'reaction_id': 3,
+            'reply': None
+        }
     ]
 }
