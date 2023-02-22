cc1b8077a26cac5108fddc8e5934bcd64fe8c830
hj60490 <harshj60490@gmail.com>
2023-02-22 09:56:21 +0530

    change model make post as necessary field

--- fb_post/models/comment_storage.py
+++ fb_post/models/comment_storage.py
@@ -9,5 +9,4 @@ class Comment(models.Model):
     commented_by_id = models.IntegerField(null=True, blank=True)
     parent_comment = models.ForeignKey('self', on_delete=models.CASCADE
                                        , null=True, blank=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True,
-                             blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
